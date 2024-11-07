75349ab79c1b2b020687fa2d530c696bd7e4b94d
Serenvilar <serenvilar.br@gmail.com>
2024-11-07 01:46:01 -0300

    subindo

--- src/validation/quarto/quartoValidation.js
+++ src/validation/quarto/quartoValidation.js
@@ -1,23 +1,23 @@
 export function validarCamposObrigatoriosQuarto(quartoObj) {
-    if (!quartoObj.tipo_quarto)
-        throw new Error('Nome do quarto obrigatório.');
+    // if (!quartoObj.tipo_quarto)
+    //     throw new Error('Nome do quarto obrigatório.');
 
-    if (!quartoObj.tamCama)
-        throw new Error('Tamanho da cama do quarto obrigatório.');
+    // if (!quartoObj.tamCama)
+    //     throw new Error('Tamanho da cama do quarto obrigatório.');
 
-    if (!quartoObj.classiAvaliacao)
-        throw new Error('Classificação de avaliação do quarto obrigatório.');
-    if (isNaN(quartoObj.classiAvaliacao))
-        throw new Error('Classificação de avaliação do quarto inválida.');
+    // if (!quartoObj.classiAvaliacao)
+    //     throw new Error('Classificação de avaliação do quarto obrigatório.');
+    // if (isNaN(quartoObj.classiAvaliacao))
+    //     throw new Error('Classificação de avaliação do quarto inválida.');
 
-    if (!quartoObj.numAvaliacao)
-        throw new Error('Numero da avaliação do quarto obrigatório.');
+    // if (!quartoObj.numAvaliacao)
+    //     throw new Error('Numero da avaliação do quarto obrigatório.');
 
-    if (quartoObj.status == undefined)
-        throw new Error('Status do quarto obrigatório.');
+    // if (quartoObj.status == undefined)
+    //     throw new Error('Status do quarto obrigatório.');
 
-    if (!quartoObj.valor)
-        throw new Error('Valor do quarto obrigatório.');
+    // if (!quartoObj.valor)
+    //     throw new Error('Valor do quarto obrigatório.');
 }
 
 export function validarQuartoUnico(registros) {
